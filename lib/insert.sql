eb7cb8d6349d42a38cf6899af2c7c010764b7194
Amy Westcott <amy.westcott@yahoo.com>
2019-07-25 18:48:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,11 +3,11 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Rowdy", 7, "male", "brown", "mean", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 6, "female", "dark brown", "friendly",, 1);
+    VALUES ("Tabitha", 6, "female", "dark brown", "friendly", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Sergeant Brown", 4, "male", "dark brown", "friendly", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 13, "female", "dark brown", "friendly",  1);
+    VALUES ("Melissa", 13, "female", "dark brown", "friendly",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Grinch", 4, "male", "dark brown", "friendly", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
